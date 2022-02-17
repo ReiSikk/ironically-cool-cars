8a8bf135898d7d46f832d7e02e09c6735ebeb5b4
Jonas Holbech <abdu3793@stud.kea.dk>
2022-02-17 13:48:37 +0100

    java

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-const url = "https://kea-alt-del.dk/t7/api/products";
-fetch(url)
-    .then(function(res) {
-        return res.json();
+const url = "https://kea21-07db.restdb.io/rest/ironicallycoolcars";
+
+const options = {
+    headers: {
+        "Init Key 2": "620e429534fd621565858722",
+    },
+};
+
+fetch(url, options)
+    .then((response) => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
     })
-    .then(function(data) {
+    .then((data) => {
         handleProductList(data);
+    })
+    .catch((e) => {
+        console.error("An error occured:", e.message);
     });
 
 function handleProductList(data) {
-    data.forEach(showProduct);
+    data.forEach((data) => {
+        console.log(data);
+    });
 }
 
 function showProduct(product) {
     console.log(product);
-    const template = document.querySelector(".").content;
+    const template = document.querySelector("#carsTemplate").content;
     const copy = template.cloneNode(true);
-    document.querySelector(".category-name").textContent = product.category;
-    copy.querySelector("h3").textContent = product.productdisplayname;
+    copy.querySelector("h2").textContent = "heyyyyyyyyy";
     const parent = document.querySelector("main");
     parent.appendChild(copy);
 }
