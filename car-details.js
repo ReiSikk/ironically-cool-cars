e98deaeda126c35a6ac0eef6c62edbed501805cb
ReiSikk <reix0010@stud.kea.dk>
2022-02-17 22:30:06 +0100

    fixed images fetching issue

--- car-details.js
+++ car-details.js
@@ -3,27 +3,27 @@ const id = urlParams.get("id");
 const url = "https://kea21-07db.restdb.io/rest/ironicallycoolcars/" + id;
 
 const options = {
-    headers: {
-        "x-apikey": "620e4b1534fd62156585872a",
-    },
+  headers: {
+    "x-apikey": "620e4b1534fd62156585872a",
+  },
 };
 fetch(url, options)
-    .then((res) => res.json())
-    .then((data) => showCars(data));
+  .then((res) => res.json())
+  .then((data) => showCars(data));
 
 function showCars(cars) {
-    console.log(cars);
-    document.querySelector(".price").textContent = cars.price + " " + "$";
-    document.querySelector(".production_year").textContent = cars.production_year;
-    document.querySelector(".colour").textContent = cars.colour;
-    document.querySelector(".brand").textContent =
-        cars.brand + " / " + cars.model;
-    document.querySelector(".legal_to_drive").textContent = cars.legal_to_drive;
-    document.querySelector(".shame_factor").textContent = cars.shame_factor;
-    document.querySelector(".description").textContent = cars.short_description;
-    document.querySelector(".seated_passengers").textContent =
-        cars.seated_passengers;
-    document.querySelector(
-        ".product-card-photo"
-    ).src = `http://gareth.dk/data/${cars.id}.jpeg`;
+  console.log(cars);
+  document.querySelector(".price").textContent = cars.price + " " + "$";
+  document.querySelector(".production_year").textContent = cars.production_year;
+  document.querySelector(".colour").textContent = cars.colour;
+  document.querySelector(".brand").textContent =
+    cars.brand + " / " + cars.model;
+  document.querySelector(".legal_to_drive").textContent = cars.legal_to_drive;
+  document.querySelector(".shame_factor").textContent = cars.shame_factor;
+  document.querySelector(".description").textContent = cars.short_description;
+  document.querySelector(".seated_passengers").textContent =
+    cars.seated_passengers;
+  document.querySelector(
+    ".product-card-photo"
+  ).src = `http://gareth.dk/data/${cars.id}.jpeg`;
 }
--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 const url = "https://kea21-07db.restdb.io/rest/ironicallycoolcars";
 
 const options = {
-    headers: {
-        "x-apikey": "620e4b1534fd62156585872a",
-    },
+  headers: {
+    "x-apikey": "620e4b1534fd62156585872a",
+  },
 };
 
 fetch(url, options)
-    .then((response) => {
-        if (!response.ok) {}
-        return response.json();
-    })
-    .then((data) => {
-        handleProductList(data);
-    });
+  .then((response) => {
+    if (!response.ok) {
+    }
+    return response.json();
+  })
+  .then((data) => {
+    handleProductList(data);
+  });
 
 function handleProductList(data) {
-    data.forEach(showCars);
+  data.forEach(showCars);
 }
 
 function showCars(cars) {
-    console.log(cars);
-    const template = document.querySelector("#carsTemplate").content;
-    const copy = template.cloneNode(true);
-    copy.querySelector(".card-header").textContent = cars.brand;
-    copy.querySelector(".brand").textContent = cars.brand + "/" + cars.model;
-    copy.querySelector(".price").textContent = "Price : " + cars.price + "$";
-    copy.querySelector(
-        ".products-card-photo"
-    ).src = `http://gareth.dk/data/${cars.id}.jpeg`;
-    copy.querySelector("a").setAttribute("href", `product.html?id=${cars._id}`);
-    const parent = document.querySelector(".yy");
-    parent.appendChild(copy);
+  console.log(cars);
+  const template = document.querySelector("#carsTemplate").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector(".card-header").textContent = cars.brand;
+  copy.querySelector(".brand").textContent = cars.brand + "/" + cars.model;
+  copy.querySelector(".price").textContent = "Price : " + cars.price + "$";
+  copy.querySelector(".products-card-photo").src = cars.imageurl;
+  copy.querySelector("a").setAttribute("href", `product.html?id=${cars._id}`);
+  const parent = document.querySelector(".yy");
+  parent.appendChild(copy);
 }
